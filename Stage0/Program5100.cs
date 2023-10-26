1ad7b036ea55375610e3ae74c775c56d6c3acd64
hb054 <hb0545304600@gmail.com>
2023-10-26 13:14:53 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program5100.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Welcome5100();
+        Welcome2542();
+
+        Console.ReadKey();
+
+
+    }
+    static partial void Welcome2542();
+
+    private static void Welcome5100()
+    {
+        Console.Write("Enter your name: ");
+        string name = "";
+        name = Console.ReadLine();
+        Console.WriteLine(name + ",welcome to my first console");
+    }
+}
