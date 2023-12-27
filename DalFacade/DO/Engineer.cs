85ee61994653afd58b24b3433936bf430cb1387c
הודיה ביטון <hb0545304600@gmail.com>
2023-12-27 15:15:20 +0200

    change with stage 3

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -11,8 +11,26 @@ public record Engineer
     int? PricePerHour
 )
 {
-    public Engineer():this(null,"","",null,null)
+    //public Dependence() : this(null, 0, 0) { }
+    //public Dependence(int? IdDependence, Dependence d) : this(IdDependence, d.NumberDependenceTask, d.NuberPreviousTask)
+    //{
+    //    this.IdDependence = IdDependence;
+    //    this.NumberDependenceTask = d.NumberDependenceTask;
+    //    this.NuberPreviousTask = d.NuberPreviousTask;
+    //}
+    public Engineer() : this(null, "", "", null, null)
     {
     }
+    //public Engineer(int? IdEngineer, string NameEngineer, string MailEnginerr, Experience? EngineerRank, int? PricePerHour) :this()
+
+    //{
+    //    this.IdEngineer = IdEngineer;
+    //    this.NameEngineer = NameEngineer;
+    //    this.MailEnginerr = MailEnginerr;
+    //    this.EngineerRank = EngineerRank;
+    //    this.PricePerHour = PricePerHour;
+    //}
+
+
 }
 
