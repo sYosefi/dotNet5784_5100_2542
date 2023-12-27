7a89ae38cfe786f69cbe2857922bb59ef52c017c
הודיה ביטון <hb0545304600@gmail.com>
2023-12-27 15:19:20 +0200

    ...stage 3

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -12,7 +12,7 @@ internal class Program
     //private static IEngineer? e_dalIEngineer = new EngineerImplementation(); //Stage 1
     //private static ITask? t_dalITask = new TaskImplementation(); //Stage 1
     //private static IDependence? d_dalIDependence = new DependenceImplementation(); //Stage 1
-     static readonly IDal s_dal=new DalList();//stage 2
+     //static readonly IDal s_dal=new DalList();//stage 2
     // static readonly IDal s_dal = new DalXml(); //stage 3
 
 
