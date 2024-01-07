58acd3b20f390af7eacd63863d940cb95cf1bc0e
shirahanayosefi <st272@R6C15>
2024-01-07 16:16:56 +0200

    CRUD check

--- BL/BlApi/IEngineer.cs
+++ BL/BlApi/IEngineer.cs
@@ -5,7 +5,7 @@ public interface IEngineer
     public IEnumerable<BO.Engineer> GetListOfEngineers();
     public BO.Engineer GetEngineerDetails(int idEng);
     public void AddEngineer(BO.Engineer eng);
-    public void RemoveEngineer(BO.Engineer eng);
+    public void RemoveEngineer(int idEng);
     public void UpdateEngineerDetails(BO.Engineer eng);
 
 
--- BL/BlImplementation/EngineerImplementation.cs
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -1,26 +1,34 @@
 ﻿using BlApi;
+using BO;
+using System;
 using System.Xml.Linq;
-
 namespace BlImplementation;
 
 internal class EngineerImplementation:IEngineer
 {
+    private List<EngineerInTask> EngineerInTaskList = new List<EngineerInTask>();
     private DalApi.IDal _dal = DalApi.Factory.Get;
     public void AddEngineer(BO.Engineer eng)
     {
-        //חסר בדיקת תקינות של מייל
-        if(eng.IdEngineer>=0&&eng.Name!=""&&eng.SalaryPerHour>0)
-        {
-            DO.Engineer doEng = new DO.Engineer(eng.IdEngineer, eng.Name, eng.Email, eng.EngineerLevel, eng.SalaryPerHour);
-        }
-        else
-        {
-            throw new Exception();
-        }
-       
         try 
         {
-            int idEng = _dal.Engineer.Create(doEng);
+            if (eng.IdEngineer >= 0 && eng.Name != "" && eng.SalaryPerHour > 0 && eng.Email?.Contains("@") == true)
+            {
+                DO.Engineer doEng = new DO.Engineer(
+                    eng.IdEngineer, 
+                    eng.Name, 
+                    eng.Email,
+                    // Convert BO.Experience to DO.Experience?
+                    (DO.Experience?)eng.EngineerLevel, 
+                    eng.SalaryPerHour
+                    ); 
+                int idEng = _dal.Engineer.Create(doEng);
+            }
+            else
+            {
+                throw new Exception();
+            }
+           
         }
         catch (DO.DalAlreayExistException) 
         {
@@ -31,33 +39,84 @@ internal class EngineerImplementation:IEngineer
 
     public BO.Engineer GetEngineerDetails(int idEng)
     {
-        DO.Engineer? doEng = _dal.Engineer.Read(????);
+        DO.Engineer? doEng = _dal.Engineer.Read(e => e.IdEngineer == idEng);
         if(doEng==null) 
         {
             //throw new Exception
+            // Handle the case where the engineer is not found, for example, throw an exception
+            throw new Exception("Engineer not found");
         }
         return new BO.Engineer()
         {
             IdEngineer = idEng,
             Name = doEng.NameEngineer,
             Email = doEng.MailEnginerr,
-            EngineerLevel = doEng.EngineerRank,
-            SalaryPerHour = doEng.PricePerHour
+            EngineerLevel = (BO.Experience)(int)Enum.Parse(typeof(BO.Experience), doEng.EngineerRank.ToString()),
+            // Use ?? with a default value, adjust as needed
+            SalaryPerHour = doEng.PricePerHour ?? 0   
         };
     }
 
     public IEnumerable<BO.Engineer> GetListOfEngineers()
     {
-        throw new NotImplementedException();
+        return (from DO.Engineer doEngineer in _dal.Engineer.ReadAll() select new BO.Engineer
+        {
+            IdEngineer = doEngineer.IdEngineer??0,
+            Name = doEngineer.NameEngineer,
+            Email = doEngineer.MailEnginerr,
+            EngineerLevel = (BO.Experience)(int)Enum.Parse(typeof(BO.Experience), doEngineer.EngineerRank.ToString()),
+            SalaryPerHour = doEngineer.PricePerHour??0
+           // CurrentTask = null
+        }) ;
     }
 
-    public void RemoveEngineer(BO.Engineer eng)
+    public void RemoveEngineer(int idEng)
     {
-        throw new NotImplementedException();
+        try
+        {
+            if (!EngineerInTaskList.Any(e => e.IdEngineer == idEng))
+            {
+                _dal.Engineer.Delete(idEng);
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+        }
+        catch (DO.DalAlreayExistException)
+        {
+            //זריקת חריגה של מהנדס קיים מה-BO 
+            // throw new BO.BlAlreadyExistsException($"Student with ID={boStudent.Id} already exists", ex);
+        }
     }
 
     public void UpdateEngineerDetails(BO.Engineer eng)
     {
-        throw new NotImplementedException();
+        try
+        {
+            if (eng.IdEngineer >= 0 && eng.Name != "" && eng.SalaryPerHour > 0 && eng.Email?.Contains("@") == true)
+            {
+                DO.Engineer doEng = new DO.Engineer(
+                    eng.IdEngineer,
+                    eng.Name,
+                    eng.Email,
+                    // Convert BO.Experience to DO.Experience?
+                    (DO.Experience?)eng.EngineerLevel,
+                    eng.SalaryPerHour
+                    );
+                _dal.Engineer.Update(doEng);
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+        }
+        catch (DO.DalAlreayExistException)
+        {
+            //זריקת חריגה של מהנדס קיים מה-BO 
+            // throw new BO.BlAlreadyExistsException($"Student with ID={boStudent.Id} already exists", ex);
+        }
     }
 }
