89fe88cc01b5c7bee0d78d3a6c1da1f68b35d8ff
הודיה ביטון <hb0545304600@gmail.com>
2024-02-18 19:48:59 +0200

    final commit stage 5

--- PL/Engineer/Engineer.xaml.cs
+++ PL/Engineer/Engineer.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,61 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace PL.Engineer
+namespace PL.Engineer;
+
+
+/// <summary>
+/// Interaction logic for Engineer.xaml
+/// </summary>
+public partial class Engineer : Window
 {
-    /// <summary>
-    /// Interaction logic for Engineer.xaml
-    /// </summary>
-    public partial class Engineer : Window
+    static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+
+    bool isAdd;
+    public BO.Engineer EngineerItem
     {
-        public Engineer()
+        get { return (BO.Engineer)GetValue(EngineerProperty); }
+        set { SetValue(EngineerProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty EngineerProperty =
+        DependencyProperty.Register("EngineerItem", typeof(BO.Engineer), typeof(Engineer), new PropertyMetadata(null));
+
+
+    public Engineer(int id=0)
+    {
+        isAdd=(id==0)?true:false;
+        
+        //EngineerItem = new BO.Engineer();
+        InitializeComponent();
+        try
         {
-            InitializeComponent();
+            EngineerItem = (id == 0) ? new BO.Engineer() : s_bl?.Engineer.GetEngineerDetails(id)!;
         }
+        catch(Exception ex) 
+        { }
+    }
+
+    private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (isAdd)
+            {
+                s_bl.Engineer.AddEngineer(EngineerItem);
+                MessageBox.Show("Engineer successfully added");
+                Close();
+            }
+            else
+            {
+                s_bl.Engineer.UpdateEngineerDetails(EngineerItem);
+                MessageBox.Show("Engineer successfully updated");
+                Close();
+            }
+        }
+        catch (Exception ex) { }
+   
     }
 }
